869f8f937a51e8574185a860652308d6f50bc572
Ajay <ajay949427@gmail.com>
2023-06-20 15:35:49 +0530

    initial commit

---
+++ main/java/com/Tcs/App.java
@@ -0,0 +1,13 @@
+package com.Tcs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
